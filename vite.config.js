2c8a881cfe4e585d314a606b121228fb69aed00a
Bella <bella.wang.10@gmail.com>
2026-08-02 07:43:12 -0700

    fixed bugs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',  // important for user pages
   plugins: [react()],
-  base: '/',
   server: {
     historyApiFallback: true,
   }
