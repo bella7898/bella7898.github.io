31b0ee790f3d5917a81e14ecc6fd47b1ca669569
Bella <bella.wang.10@gmail.com>
2026-08-06 20:18:52 -0700

    added glitch effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero(){
     return(
         <div id="hero" className="sections hero-extended">
             <div className="hero-text">
-                <h1>Bella Wang</h1> 
+                <h1 className="glitch" data-text="Bella Wang">Bella Wang</h1> 
                 <p>Electrical Engineering Student @ UBC</p>
                 <div>
                     <button><a href="#projects">View Projects</a></button>
